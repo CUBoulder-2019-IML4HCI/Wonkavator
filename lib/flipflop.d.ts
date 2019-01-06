c86130b146ca076adccd06446cb28d087710c9eb
Ben Shapiro <ben.shapiro@colorado.edu>
2019-01-06 21:19:22 +0000

    first commit

---
+++ lib/flipflop.d.ts
@@ -0,0 +1,10 @@
+declare var five: any;
+declare var Raspi: any;
+declare var board: any;
+declare var relayPinNumber: string;
+declare var relayPin: any;
+declare var relayPinState: boolean;
+declare function relayOff(): void;
+declare function relayOn(): void;
+declare function relayToggle(): void;
+//# sourceMappingURL=flipflop.d.ts.map
---
+++ src_old/flipflop.js
@@ -0,0 +1,31 @@
+var five = require("johnny-five");
+var Raspi = require("raspi-io");
+var board = new five.Board({
+  io: new Raspi()
+});
+
+var relayPinNumber = "GPIO26";
+var relayPin = new five.Relay(relayPinNumber);
+var relayPinState = null;
+
+function relayOff(){
+    relayPin.open();
+    relayPinState = false;
+}
+
+function relayOn(){
+    relayPin.close();
+    relayPinState = true;
+}
+
+function relayToggle(){
+    relayPinState ? relayOff() : relayOn();
+}
+
+
+board.on("ready", function() {
+    relayOff();
+    setInterval(relayToggle, 1000);
+});
+
+
