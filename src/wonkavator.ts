b43d9732989e8615f983310887c330d6b1ed609f
Ben Shapiro <ben.shapiro@colorado.edu>
2019-01-23 22:41:27 +0000

    added OSC handlers

--- src/wonkavator.ts
+++ src/wonkavator.ts
@@ -4,10 +4,12 @@ import { Relay, Board } from "johnny-five";
 
 let five = require("johnny-five");
 let Raspi = require("raspi-io");
+let OSC = require("osc-js");
 
 
-let osc = require("osc-js");
-
+///////////////
+/// Defining our button pushing mechanism
+///////////////
 
 let board:Board = new five.Board({
     io: new Raspi()
@@ -26,28 +28,33 @@ class J5Relay{
         this.off();
     }
 
+    // Turn on the relay
     on(){
         this.pin.close();
         this.state = true;        
     }
 
+    // Turn off the relay
     off(){
         this.pin.open();
         this.state = false;
     }
 
+    // Flip the state of the relay on -> off, or off -> on
     toggle(){
         this.state ? this.off() : this.on();
     }
 
+    pressAndRelease(){
+        this.on();
+        setTimeout(this.off, 250)
+    }
 }
 
 console.log("Board id: " + board.id)
 console.log("Pin count: " + board.pins.length)
 
-
 let relays = { up: new J5Relay("GPIO26"), down: new J5Relay("GPIO29") };
-
 
 function toggleRelays(){
     relays.up.toggle();
@@ -59,3 +66,19 @@ board.on("ready", function() {
 });
 
 
+//////////
+// Putting our buttons on the network
+//////////
+
+const osc = new OSC()
+
+osc.on('/up', (message: any) => {
+    relays.up.pressAndRelease();
+})
+
+
+osc.on('/down', (message: any) => {
+    relays.down.pressAndRelease();
+})
+
+osc.open({ port: 9009})
